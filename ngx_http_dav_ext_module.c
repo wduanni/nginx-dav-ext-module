72406a2577bf04ea4c2337fcc1e637f293316636
WDN <78674458+wduanni@users.noreply.github.com>
2025-04-05 22:32:36 +0800

    Update ngx_http_dav_ext_module.c

--- ngx_http_dav_ext_module.c
+++ ngx_http_dav_ext_module.c
@@ -1066,15 +1066,18 @@ ngx_http_dav_ext_propfind_response(ngx_http_request_t *r, ngx_array_t *entries,
                                                      entry[n].uri.len,
                                                      NGX_ESCAPE_URI_COMPONENT)
                            - p;
-         for(size_t i = 0; entry[n].uri.len > 2 && i <= entry[n].uri.len - 3; i++){
-            if (p[i] == '%' && p[i + 1] == '2' && p[i + 2] == 'F'){
-                ngx_cpystrn(p + i + 1, p + i + 3, entry[n].uri.len - i);
-
+                           
+        size_t len = entry[n].uri.len;
+        for (size_t i = 0; len > 2 && i <= len - 3; i++) {
+            if (p[i] == '%' && p[i + 1] == '2' && p[i + 2] == 'F') {
+                ngx_cpystrn(p + i + 1, p + i + 3, len - i);
                 p[i] = '/';
-                p[entry[n].uri.len--] = 0;
-                p[entry[n].uri.len--] = 0;
+                p[--len] = 0;
+                p[--len] = 0;
             }
         }
+        entry[n].uri.len = len;
+                           
 
         entry[n].uri.data = p;
     }
